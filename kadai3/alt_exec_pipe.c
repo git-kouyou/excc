7a6c3c4a9a24b41a2c936e3449130923ed112cbc
kouyou <98743479+git-kouyou@users.noreply.github.com>
2026-06-20 13:12:50 +0900

    プログラムの修正

--- kadai3/alt_exec_pipe.c
+++ kadai3/alt_exec_pipe.c
@@ -4,14 +4,19 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-#define MAX_NUM 100
-
-int main() {
+int main(int argc, char *argv[]) {
     int toParent[2];
     int toChild[2];
     pipe(toParent);
     pipe(toChild);
     int buf = 0;
+    int max_num = 10;
+
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+        exit(1);
+    }
+    max_num = atoi(argv[1]);
 
     int pid = fork();
     if(pid < 0) {
@@ -23,7 +28,7 @@ int main() {
         close(toParent[0]);
         close(toChild[1]);
         write(toParent[1], &buf, sizeof(buf));
-        for(int i = 1; i <= MAX_NUM; i++) {
+        for(int i = 1; i <= max_num; i++) {
             read(toChild[0], &buf, sizeof(buf));
             printf("Child: %d\n", i);
             write(toParent[1], &buf, sizeof(buf));
@@ -32,7 +37,7 @@ int main() {
     } else {
         close(toParent[1]);
         close(toChild[0]);
-        for(int i = 1; i <= MAX_NUM; i++) {
+        for(int i = 1; i <= max_num; i++) {
             read(toParent[0], &buf, sizeof(buf));
             printf("Parent: %d\n", i);
             write(toChild[1], &buf, sizeof(buf));
